eb982aebd252619bda046056be4f971ca58bba7f
SeBed13 <bedregianusebastian@gmail.com>
2023-04-24 18:03:36 +0300

    Changed to do every operation with integer numbers.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,26 @@
-public class Main {
-public static void printAndCalculateDiffOfTwoNumbers(int primulNumar, int alDoileaNumar) {
-    System.out.println("The difference between " + primulNumar + " - " + alDoileaNumar + " is " + (primulNumar-alDoileaNumar));
-}
-    public static int printAndCalculateSumOfTwoNumbers(int firstNumber, int secondNumber) {
-        System.out.println("Numbers being added up " + firstNumber + " + " + secondNumber + " with the result being " + (firstNumber+secondNumber));
-        return firstNumber + secondNumber;
-    }
-    public static void main(String[] args) {
-printAndCalculateSumOfTwoNumbers(5, 7);
-printAndCalculateSumOfTwoNumbers(10, 112);
-printAndCalculateSumOfTwoNumbers(4578, 8947);
-printAndCalculateSumOfTwoNumbers(89376, 74893);
-printAndCalculateDiffOfTwoNumbers(7, 3);
-printAndCalculateDiffOfTwoNumbers(146, 78);
-printAndCalculateDiffOfTwoNumbers(64783, 2832);
-printAndCalculateDiffOfTwoNumbers(997763, 66378);
+import java.util.Scanner;
 
-int sum = 0;
-for (int i = 0; i < 10; i++){
-    sum = printAndCalculateSumOfTwoNumbers(i, i + 2);
-}
-        System.out.println("Sum is: " + sum);
+public class Main {
+    public static int printAndCalculateEverythingOfTwoNumbers() {
+        Scanner no = new Scanner(System.in);
+        System.out.println("Please write two numbers:");
+
+        int firstNumber = no.nextInt();
+        int secondNumber = no.nextInt();
+
+        System.out.println("Numbers being added up " + firstNumber + " + " + secondNumber + " with the result being " + (firstNumber + secondNumber));
+
+        System.out.println("The difference between " + firstNumber + " - " + secondNumber + " is " + (firstNumber - secondNumber));
+
+        System.out.println("The multiplication between " + firstNumber + " * " + secondNumber + " is " + (firstNumber * secondNumber));
+
+        System.out.println("The division between " + firstNumber + " / " + secondNumber + " is " + (firstNumber / secondNumber));
+
+        return firstNumber;
+    }
+
+    public static void main(String[] args) {
+
+printAndCalculateEverythingOfTwoNumbers();
     }
 }
